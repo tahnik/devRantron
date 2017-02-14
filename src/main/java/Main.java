572dc9605fd782d91e418b3bf1ce70e02bace7a6
Tahnik Mustasin <tahnik@live.co.uk>
2017-02-14 19:53:25 +0000

    Added Main UI Prototype

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/window_main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/window_main_pane.fxml"));
         primaryStage.setTitle("devRant Unofficial");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
