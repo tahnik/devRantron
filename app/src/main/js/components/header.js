3125d8ec7441ee636fc44f0e9c794899efb05c35
Tahnik Mustasin <tahnik@live.co.uk>
2017-03-19 20:05:17 +0000

    Top static bar looks nice!

--- app/src/main/js/components/header.js
+++ app/src/main/js/components/header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import StaticSideNav from '../containers/static_side_nav';
+import TopNav from '../containers/top_nav';
 
 class Header extends Component {
 	render(){
 		return(
-			<div style={{ height: '36px', backgroundColor: '#40415A'}}>
+			<div className="header">
+				<TopNav />
+				<StaticSideNav />
 			</div>
 		)
 	}
