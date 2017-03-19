f42693a6dcddddc034ef2ea63cf263524b2cc4c3
Tahnik Mustasin <tahnik@live.co.uk>
2017-03-19 18:28:58 +0000

    moved sidebar to nav

--- app/src/main/js/components/header.js
+++ app/src/main/js/components/header.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import SideNav from '../containers/sidenav';
 
 class Header extends Component {
 	render(){
 		return(
 			<div style={{ marginTop: 20 }}>
 				<h1>React Redux Router</h1>
-				<SideNav />
 			</div>
 		)
 	}
--- app/src/main/js/routes.js
+++ app/src/main/js/routes.js
@@ -5,18 +5,51 @@ import Header from './components/header';
 import Main from './components/main';
 
 import {
-  HashRouter as Router,
-  Route,
+	HashRouter as Router,
+	Route,
 } from 'react-router-dom';
 
 export default class extends Component {
+	componentDidMount() {
+		$(".button-collapse").sideNav();
+	}
 	render() {
 		return (
 			<Router>
 				<div>
-					<Header />
-					<Route exact path="/" component={Main} />
-					<Route path="/view/:name" component={ViewItem} />
+					<ul id="slide-out" className="side-nav">
+						<li>
+							<div className="userView">
+								<a href="#!name">
+									<span className="white-text name">John Doe</span>
+								</a>
+								<a href="#!email">
+									<span className="white-text email">jdandturk@gmail.com</span>
+								</a>
+							</div>
+						</li>
+						<li>
+							<a href="#!">
+								<i className="material-icons">cloud</i>
+								First Link With Icon
+            </a>
+						</li>
+						<li>
+							<a href="#!">Second Link</a>
+						</li>
+						<li>
+							<div className="divider"></div>
+						</li>
+						<li>
+							<a className="subheader">Subheader</a>
+						</li>
+						<li>
+							<a className="waves-effect" href="#!">Third Link With Waves</a>
+						</li>
+					</ul>
+					<a href="#" data-activates="slide-out" className="button-collapse btn">
+						<i className="material-icons">menu</i>
+					</a>
 				</div>
 			</Router>
 		)
