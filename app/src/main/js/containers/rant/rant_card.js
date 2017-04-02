658cac3b2d7b86b38ca776336c2953541998c239
Tahnik Mustasin <tahnik@live.co.uk>
2017-04-02 20:37:48 +0100

    Testing new width, changed ++ button, added whitespace

--- app/src/main/js/containers/rant/rant_card.js
+++ app/src/main/js/containers/rant/rant_card.js
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 
 class RantCard extends Component {
   render() {
-    if (this.props.rant.attached_image !== '') {
-      return (
-        <div className="rant_card row" >
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <p>{this.props.rant.text}</p>
-            </div>
-            <div className="card-image" style={{ background: `url(${this.props.rant.attached_image.url}) no-repeat center center` }}>
-              <div><i className="ion-arrow-expand" /></div>
-            </div>
-            <div className="card-bottomBar">
-              <i className="ion-plus-round" />
-              <p>{this.props.rant.score}</p>
-              <i className="ion-minus-round" />
-              <div style={{ flex: 1 }} />
-              <p>{this.props.rant.num_comments}</p>
-              <i className="ion-chatbubbles" />
-            </div>
-          </div>
-        </div>
-      );
-    }
     return (
       <div className="rant_card row" >
         <div className="card blue-grey darken-1">
           <div className="card-content white-text">
-            <p>{this.props.rant.text}</p>
+            <pre><p>{this.props.rant.text}</p></pre>
+          </div>
+          <div className="card-image">
+            <img src={this.props.rant.attached_image.url} alt="" />
           </div>
           <div className="card-bottomBar">
             <i className="ion-plus-round" />
