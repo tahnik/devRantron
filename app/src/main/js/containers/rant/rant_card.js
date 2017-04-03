2b9df53e86821c028edc810853c1fe3bc6581409
Unknown <haslun16@edu.vilhelmina.se>
2017-04-03 12:35:42 +0200

    Added user inforamtion on top of rant card.

--- app/src/main/js/containers/rant/rant_card.js
+++ app/src/main/js/containers/rant/rant_card.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-
+/* API Ref:
+attached_image: ""
+created_time: 1491178991
+edited: false
+id: 509276
+num_comments: 3
+num_downvotes: 0
+num_upvotes: 40
+score: 40
+tags: Array[]
+text: ""
+user_avatar: Object
+user_id: 49546
+user_score: 3506
+user_username: "DLMousey"
+vote_state: 0
+*/
 class RantCard extends Component {
   render() {
     return (
       <div className="rant_card row" >
         <div className="card blue-grey darken-1">
+          <div className="card-user">
+            <img src={`https://avatars.devrant.io/${this.props.rant.user_avatar.i}`}/>
+            <div>
+              <p>{this.props.rant.user_username}</p>
+              <p className="user_score">+{this.props.rant.user_score}</p>
+            </div>
+          </div>
           <div className="card-content white-text">
             <pre><p>{this.props.rant.text}</p></pre>
           </div>
