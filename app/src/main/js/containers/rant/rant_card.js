db5d77810fd1225221e7d7d6b0d85a62e0834482
Tahnik Mustasin <tahnik@live.co.uk>
2017-04-04 15:55:13 +0100

    Fixed some linting errors

--- app/src/main/js/containers/rant/rant_card.js
+++ app/src/main/js/containers/rant/rant_card.js
@@ -18,28 +18,36 @@ vote_state: 0
 */
 class RantCard extends Component {
   render() {
+    const { rant } = this.props;
+    let trimmedString = rant.text;
+    if (rant.text.length > 300) {
+      const maxLength = 300;
+      trimmedString = rant.text.substr(0, maxLength);
+      trimmedString = `${trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(' ')))}...(Read More)`;
+    }
+    console.log(rant);
     return (
       <div className="rant_card row" >
         <div className="card blue-grey darken-1">
           <div className="card-user">
-            <img src={`https://avatars.devrant.io/${this.props.rant.user_avatar.i}`}/>
+            <img src={`https://avatars.devrant.io/${rant.user_avatar.i}`} alt="" />
             <div>
-              <p>{this.props.rant.user_username}</p>
-              <p className="user_score">+{this.props.rant.user_score}</p>
+              <p>{rant.user_username}</p>
+              <p className="user_score">+{rant.user_score}</p>
             </div>
           </div>
           <div className="card-content white-text">
-            <pre><p>{this.props.rant.text}</p></pre>
+            <pre><p>{trimmedString}</p></pre>
           </div>
           <div className="card-image">
-            <img src={this.props.rant.attached_image.url} alt="" />
+            <img src={rant.attached_image.url} alt="" />
           </div>
           <div className="card-bottomBar">
             <i className="ion-plus-round" />
-            <p>{this.props.rant.score}</p>
+            <p>{rant.score}</p>
             <i className="ion-minus-round" />
             <div style={{ flex: 1 }} />
-            <p>{this.props.rant.num_comments}</p>
+            <p>{rant.num_comments}</p>
             <i className="ion-chatbubbles" />
           </div>
         </div>
@@ -47,5 +55,11 @@ class RantCard extends Component {
     );
   }
 }
+
+RantCard.propTypes = {
+  rant: React.PropTypes.shape({
+    text: React.PropTypes.string,
+  }).isRequired,
+};
 
 export default RantCard;
