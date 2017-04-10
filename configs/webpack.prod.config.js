d578109c4c69a7764e398831a23e8111185bdf33
SirWindfield <sven.lechner@rwth-aachen.de>
2017-04-10 12:47:41 +0200

    Enabled minification

--- configs/webpack.prod.config.js
+++ configs/webpack.prod.config.js
@@ -48,7 +48,7 @@ module.exports = {
     ],
   },
   plugins: [
-    // new BabiliPlugin(),even disabled minification...
+    new BabiliPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
