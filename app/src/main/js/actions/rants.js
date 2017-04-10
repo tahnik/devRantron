9650b02f12816633d69e525456c3bb7420a32b93
SirWindfield <sven.lechner@rwth-aachen.de>
2017-04-10 11:34:33 +0200

    Disabled API compression

--- app/src/main/js/actions/rants.js
+++ app/src/main/js/actions/rants.js
@@ -3,7 +3,8 @@ import { FETCH_RANTS, RESET_PAGE } from '../consts/rants';
 import { STATE } from '../consts/state';
 
 // change to disable comprssion
-rantscript.httpSettings.SET_COMPRESS(true);
+rantscript.httpSettings.SET_COMPRESS(false);
+rantscript.httpSettings.SET_DEBUG(true);
 
 export function fetch(type, amount, page = 0) {
   return (dispatch) => {
