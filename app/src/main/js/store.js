a35a5187522bb695d97df074c96c34052c48a42b
SirWindfield <svenlechner@outlook.de>
2017-04-12 01:57:36 +0200

    Fixed store and index linting

--- app/src/main/js/store.js
+++ app/src/main/js/store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
 import reducers from './reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //eslint-disable-line
 const middleware = applyMiddleware(thunk);
 
 export default createStore(reducers, composeEnhancers(
