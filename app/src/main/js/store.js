2b18d18253633b834d670caf0499d0ef34af554b
Tahnik Mustasin <tahnik@live.co.uk>
2017-04-15 19:23:47 +0100

    Fixed auth bug

--- app/src/main/js/store.js
+++ app/src/main/js/store.js
@@ -5,7 +5,11 @@ import reducers from './reducers/index';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //eslint-disable-line
 const middleware = applyMiddleware(thunk);
 
-const initialAuthState = JSON.parse(localStorage.getItem('auth'));
+let initialAuthState = JSON.parse(localStorage.getItem('auth'));
+
+if (initialAuthState === null) {
+  initialAuthState = {};
+}
 
 const initialState = {
   auth: initialAuthState,
