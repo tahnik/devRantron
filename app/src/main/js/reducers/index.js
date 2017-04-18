d6db006435d934216ab6a566aac6283934b3d381
Saurabh <unknown7bolt@gmail.com>
2017-04-18 15:50:51 +0530

    modified index.js reducer to include nav

--- app/src/main/js/reducers/index.js
+++ app/src/main/js/reducers/index.js
@@ -3,12 +3,14 @@ import theme from './settings';
 import rants from './rants';
 import rant from './rant';
 import auth from './auth';
+import { TopNavItems } from './nav';
 
 const rootReducer = combineReducers({
   theme,
   rants,
   rant,
   auth,
+  topNavItems: TopNavItems,
 });
 
 export default rootReducer;
