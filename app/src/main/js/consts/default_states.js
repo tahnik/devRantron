dd80834fb16af344f55e3a6432cc674b42607f3c
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-13 22:23:50 +0100

    Added default state which holds the default state for all components

---
+++ app/src/main/js/consts/default_states.js
@@ -0,0 +1,16 @@
+export default {
+  AUTH: {
+    user: null,
+    state: 'STATE_INITIAL',
+  },
+  SETTINGS: {
+    theme: {
+      name: 'Dark Theme',
+      backgroundColor: '#40415A',
+    },
+  },
+  TOAST: {
+    text: '',
+    isVisible: false,
+  },
+};
