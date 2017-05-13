17b0307af9aa222d742f317c92327828d4084fe7
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-13 22:22:26 +0100

    persistant store now listens for changes

--- app/src/main/js/store.js
+++ app/src/main/js/store.js
@@ -1,28 +1,24 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
-import { DEFAULT_STATE } from './reducers/auth';
-import { INITIAL_STATE } from './reducers/settings';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //eslint-disable-line
 const middleware = applyMiddleware(thunk);
 
-let initialAuthState = JSON.parse(localStorage.getItem('auth'));
-let initialSettingsState = JSON.parse(localStorage.getItem('settings'));
-
-if (initialAuthState === null) {
-  initialAuthState = DEFAULT_STATE;
-}
-
-if (initialSettingsState === null) {
-  initialSettingsState = INITIAL_STATE;
-}
-
-const initialState = {
-  auth: initialAuthState,
-  settings: initialSettingsState,
+const initialState = () => {
+  const persistedState = localStorage.getItem('reduxState');
+  if (persistedState) {
+    return JSON.parse(persistedState);
+  }
+  return {};
 };
 
-export default createStore(reducers, initialState, composeEnhancers(
+const store = createStore(reducers, initialState(), composeEnhancers(
     middleware,
 ));
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
+
+export default store;
