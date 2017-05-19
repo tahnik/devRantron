ef2e1a6b9b6d998c7916a156eeeaf1ab62e82dbd
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-19 23:41:05 +0100

    Added the notif component

--- app/src/main/js/components/notifs/notifs.js
+++ app/src/main/js/components/notifs/notifs.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import rantscript from '../../consts/rantscript';
+import { STATE } from '../../consts/types';
 
 class Notifs extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      middleWidth: null,
+      notifTimestamp: 1,
     };
   }
   componentDidMount() {
-    //
+    /*
+     * In our container, which connected this component to redux,
+     * we asked for auth state and said that we will dispatch notifs actions.
+     * That's why we can access them as props in here.
+     */
+    const { auth, fetchNotifs } = this.props;
+
+    // Check if there's any authenticated user before doing anything
+    if (auth.user) {
+      /* We dispatch an action immedietly saying that notifs is loading
+       * This may not be used now, might come handy in future
+       */
+      fetchNotifs(null, STATE.LOADING);
+
+
+      rantscript
+      .notifications(auth.user.authToken, this.state.notifTimestamp)
+      .then((res) => {
+        /*
+         * We have got a successful response, let's dispatch to let
+         * redux store know about it
+         */
+        fetchNotifs(res, STATE.SUCCESS);
+
+        // We update our components state
+        this.setState({ notifTimestamp: res.check_time });
+      })
+      .catch(() => {
+        // Just in case it fails, we dispatch a failure
+        fetchNotifs(null, STATE.FAILED);
+      });
+    }
   }
   render() {
+    const { notifs } = this.props;
+
+    /* Wondering why there is notifs.notifs?
+     * If you look at the default state, it looks like this:
+     * const NOTIFS = { state: STATE.INITIAL, notifs: {} }
+     * so in redux store, it's stored like this:
+     * { ...other states, notifs: { state: STATE.INITIAL, notifs: {} } }
+     * So in our container, when we ask redux for notifs, it actually gives
+     * use this:
+     * notifs: { state: STATE.INITIAL, notifs: {} }
+     *
+     * Also, we check if notif actually exists by checking notifs.notifs.data
+     * otherwise we don't show any numbers yet
+     */
+    const data = notifs.notifs.data;
     return (
       <div className="notifs_container" >
-        Notifs
+        { data ? data.num_unread : null }
       </div>
     );
   }
 }
 
 Notifs.propTypes = {
+  auth: PropTypes.object.isRequired,
+  fetchNotifs: PropTypes.func.isRequired,
+  notifs: PropTypes.object.isRequired,
 };
 
 export default Notifs;
