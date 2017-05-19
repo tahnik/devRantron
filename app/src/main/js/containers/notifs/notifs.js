2e89c3d03773eff7e43d658bb1fad2afb4c30910
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-19 23:04:41 +0100

    Added container for notif

--- app/src/main/js/containers/notifs/notifs.js
+++ app/src/main/js/containers/notifs/notifs.js
@@ -1,15 +1,29 @@
 import { connect } from 'react-redux';
 import Notifs from '../../components/notifs/notifs';
+import { fetchNotifs } from '../../actions/notifs';
 
-// const mapStateToProps = state => ({
+/* Here we ask redux to give us this states
+ * auth is needed as we will only show notif if the user is authenticated
+ */
+const mapStateToProps = state => ({
+  auth: state.auth,
+  notifs: state.notifs,
+});
 
-// });
+/* Here we use dispatch to dispatch actions to redux store
+ *
+ */
+const mapDispatchToProps = dispatch => ({
+  fetchNotifs: () => {
+    dispatch(fetchNotifs());
+  },
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   // login: (username, password) => {
-//   //   dispatch(login(username, password));
-//   // },
-// });
-
-export default connect(null, null)(Notifs);
+/* We use higher order component to connect our notif component with redux
+ * Basically we are saying that: Redux, I need these state from your
+ * store (mapStateToProps), and I will dispatch these actions to your
+ * store (mapDispatchToProps). Please make them available to our notif
+ * component
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(Notifs);
 
