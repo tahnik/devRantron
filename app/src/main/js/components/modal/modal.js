56d631bdb2eddfc8b391f2c19ae3a567f7ea3114
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-24 23:02:17 +0100

    Added animatio to modal opening

--- app/src/main/js/components/modal/modal.js
+++ app/src/main/js/components/modal/modal.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Item from '../item/item';
 
 class Modal extends Component {
   getItem() {
     const { item, theme, vote, auth } = this.props;
-    return <Item theme={theme} cardItem={item} vote={vote} auth={auth} />;
+    return <Item key={item.id} theme={theme} cardItem={item} vote={vote} auth={auth} />;
   }
   render() {
     const { item, close } = this.props;
-    if (!item) {
-      return null;
-    }
     return (
-      <div className="modal_container">
-        <div className="close_modal" onClick={() => close()}>
-          <i className="ion-close-round" />
-        </div>
-        { this.getItem() }
-      </div>
+      <CSSTransitionGroup
+        transitionName="fade_item"
+        transitionEnterTimeout={250}
+        transitionLeaveTimeout={150}
+        className="modal"
+      >
+        {
+          item ?
+            <div className="modal_container">
+              <div className="close_modal" onClick={() => close()}>
+                <i className="ion-close-round" />
+              </div>
+              { this.getItem() }
+            </div>
+          : null
+        }
+      </CSSTransitionGroup>
+
     );
   }
 }
