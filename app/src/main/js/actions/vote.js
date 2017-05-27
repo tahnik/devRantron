483ee38e0a3b32580f4b365dd7a5e90e9cf6db81
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-27 13:52:37 +0100

    Fixed showToast in vote and commented out vote action

--- app/src/main/js/actions/vote.js
+++ app/src/main/js/actions/vote.js
@@ -3,20 +3,22 @@ import { ITEM, AUTH, FEED } from '../consts/types';
 import showToast from './toast';
 
 
+/**
+ * Votes a rant. If there is not user, shows the login page
+ *
+ * @param {number} [voteState=1] Always upvote unless specified otherwise
+ * @param {number} rantID ID of the rant to upvote (works for collab too)
+ */
 const voteRant = (voteState = 1, rantID) => (dispatch, getState) => {
   const { user } = getState().auth;
   let authToken = null;
   if (user) {
     authToken = user.authToken;
   } else {
-    dispatch(showToast(dispatch, 'Log in to vote'));
+    dispatch(showToast('Log in to vote'));
     dispatch({
       type: AUTH.NOLOGIN,
       payload: false,
-    });
-    // annoying?
-    dispatch({
-      type: FEED.ACTION.RESET,
     });
     return;
   }
@@ -29,6 +31,12 @@ const voteRant = (voteState = 1, rantID) => (dispatch, getState) => {
       });
 };
 
+/**
+ * Votes a comment. If there is not user, shows the login page
+ *
+ * @param {number} [voteState=1] Always upvote unless specified otherwise
+ * @param {number} commentID ID of the comment
+ */
 const voteComment = (voteState = 1, commentID) => (dispatch, getState) => {
   const { user } = getState().auth;
   let authToken = null;
@@ -55,14 +63,23 @@ const voteComment = (voteState = 1, commentID) => (dispatch, getState) => {
       });
 };
 
-const vote = (voteState = 1, id, type) => {
+/**
+ * Reusability. Determines what to vote
+ *
+ * @param {number} [voteState=1] Always upvote unless specified otherwise
+ * @param {any} id id of the comment or rant
+ * @param {any} type If it is a comment or rant
+ */
+const vote = (voteState = 1, id, type) => (dispatch) => {
   switch (type) {
     case ITEM.RANT.NAME:
-      return voteRant(voteState, id);
+      dispatch(voteRant(voteState, id));
+      break;
     case ITEM.COMMENT.NAME:
-      return voteComment(voteState, id);
+      dispatch(voteComment(voteState, id));
+      break;
     default:
-      return voteRant(voteState, id);
+      dispatch(voteRant(voteState, id));
   }
 };
 
