237b041bfe9adfb8f68f19ddc108a2d465be7382
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-27 13:40:00 +0100

    commented out toast

--- app/src/main/js/actions/toast.js
+++ app/src/main/js/actions/toast.js
@@ -1,7 +1,14 @@
 import { TOAST } from '../consts/types';
 
+
 let timer = null;
-export default (text, timeout = 4000) => (dispatch) => {
+/**
+ * Shows a toast (can be error, info etc)
+ *
+ * @param {any} text Text to show in the toast
+ * @param {number} [timeout=4000] Can make the toast longer or shorter
+ */
+const showToast = (text, timeout = 4000) => (dispatch) => {
   if (timer) {
     clearTimeout(timer);
   }
@@ -15,3 +22,5 @@ export default (text, timeout = 4000) => (dispatch) => {
     });
   }, timeout);
 };
+
+export { showToast as default };
