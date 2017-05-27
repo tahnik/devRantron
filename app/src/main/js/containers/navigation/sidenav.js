a29cc7e2aa4c47df28e3cc6152615a1b5d3a9c9f
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-27 18:07:53 +0100

    Changed custom column

--- app/src/main/js/containers/navigation/sidenav.js
+++ app/src/main/js/containers/navigation/sidenav.js
@@ -10,7 +10,7 @@ const SIDE_NAV_ITEMS = [
   { name: 'Rants', route: ROUTES.rants, icon: 'ion-chatboxes' },
   { name: 'Collabs', route: ROUTES.collabs, icon: 'ion-person-stalker' },
   { name: 'Stories', route: ROUTES.stories, icon: 'ion-ios-bookmarks' },
-  { name: 'Custom', route: ROUTES.custom, icon: 'ion-ios-bookmarks' },
+  { name: 'Custom', route: ROUTES.custom, icon: 'ion-edit' },
 ];
 
 const mapDispatchToProps = dispatch => ({
