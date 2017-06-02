bac6cdf7ba9cebf1ba2f0a5d3ffc4956bb108bef
hampus <haslun16@edu.vilhelmina.se>
2017-06-03 01:54:31 +0200

    Baisc user card

--- app/src/main/js/components/user/profile.js
+++ app/src/main/js/components/user/profile.js
@@ -2,7 +2,37 @@ import React, { Component } from 'react';
 
 class Profile extends Component {
   render() {
-    return <div>Profile</div>;
+    const { user } = this.props;
+    const profile = user.profile;
+    let imgsrc = '';
+    if (profile.avatar.i) {
+      imgsrc += `https://avatars.devrant.io/${profile.avatar.i}`.toString();
+    }
+    console.log(user)
+    return (
+      <div className="profile">
+        <div className="profile_header">
+          <div
+            className="profile_image"
+            style={{ background: 'url(./res/images/profile_banner.png)' }}
+          >
+            <img className="user_image" src={imgsrc} style={{ background: `#${profile.avatar.b}` }} alt="avatar" />
+            <div className="user_bg_tint" style={{ background: `#${profile.avatar.b}` }} />
+          </div>
+          <ul className="profile_description">
+            <div className="profile_username">
+              <span>{profile.username}</span>
+              <p className="user_score">+{profile.score}</p>
+            </div>
+            {profile.about && <li><i className="ion-person" /><p>{profile.about}</p></li>}
+            {profile.skills && <li><i className="ion-code" /><p>{profile.skills}</p></li>}
+            {profile.github && <li><i className="ion-social-github" /><p>{profile.github}</p></li>}
+            {profile.website && <li><i className="ion-earth" /><p>{profile.website}</p></li>}
+            {profile.location && <li><i className="ion-location" /><p>{profile.location}</p></li>}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
