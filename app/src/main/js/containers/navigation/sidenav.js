9a9b2606e61b573a77ef035083e522124d873976
hampus <haslun16@edu.vilhelmina.se>
2017-07-22 01:44:24 +0200

    Changed settings icon (I think it looks better)

--- app/src/main/js/containers/navigation/sidenav.js
+++ app/src/main/js/containers/navigation/sidenav.js
@@ -13,7 +13,7 @@ const SIDE_NAV_ITEMS = [
   { name: 'Collabs', route: ROUTES.collabs, icon: 'ion-person-stalker' },
   { name: 'Stories', route: ROUTES.stories, icon: 'ion-ios-bookmarks' },
   { name: 'Custom', route: ROUTES.custom, icon: 'ion-edit' },
-  { name: 'Settings', route: ROUTES.settings, icon: 'ion-settings' },
+  { name: 'Settings', route: ROUTES.settings, icon: 'ion-gear-a' },
 ];
 
 const mapDispatchToProps = dispatch => ({
