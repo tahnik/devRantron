44055ca98a72ddd36123b24b4b8a697552c8098e
Tahnik Mustasin <tahnik@live.co.uk>
2017-07-27 18:06:52 +0100

    added mention

--- app/src/main/js/components/utilities/ContentEditable.js
+++ app/src/main/js/components/utilities/ContentEditable.js
@@ -7,6 +7,8 @@ import EmojiPicker from '../emoji_picker/emoji_picker';
 let active = false;
 let pos = 0;
 
+let component = null;
+
 class ContentEditable extends Component {
   constructor(props) {
     super(props);
@@ -19,10 +21,70 @@ class ContentEditable extends Component {
         right: '0px',
       },
       mentions: [],
+      selectedMention: 0,
     };
   }
   shouldComponentUpdate() {
     return true;
+  }
+  componentDidMount() {
+    component = this;
+    document.addEventListener('keydown', ContentEditable.handleArrowKeys, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', ContentEditable.handleArrowKeys, false);
+  }
+  static handleArrowKeys(e) {
+    const { mentions, selectedMention } = component.state;
+    const lastIndex = mentions.length - 1;
+    switch (e.keyCode) {
+      case 9:
+        if (active) {
+          e.preventDefault();
+          if (selectedMention === lastIndex) {
+            component.setState({ selectedMention: 0 });
+            return;
+          }
+          component.setState({ selectedMention: selectedMention + 1 });
+        }
+        break;
+      case 13: {
+        if (active) {
+          e.preventDefault();
+          component.addMention();
+        }
+        break;
+      }
+      case 38: {
+        if (active) {
+          e.preventDefault();
+          if (selectedMention === 0) {
+            component.setState({ selectedMention: lastIndex });
+            return;
+          }
+          component.setState({ selectedMention: selectedMention - 1 });
+        }
+        break;
+      }
+      case 39:
+        if (active) {
+          e.preventDefault();
+          component.addMention();
+        }
+        break;
+      case 40: {
+        if (active) {
+          e.preventDefault();
+          if (selectedMention === lastIndex) {
+            component.setState({ selectedMention: 0 });
+            return;
+          }
+          component.setState({ selectedMention: selectedMention + 1 });
+        }
+        break;
+      }
+      default:
+    }
   }
   componentDidUpdate() {
     const cursor = document.getElementById('cursor');
@@ -34,41 +96,59 @@ class ContentEditable extends Component {
       mentions.style.left = `${parseInt(window.getComputedStyle(cursor).left, 10) + 5}px`;
     }
   }
+  addMention() {
+    const { mentions, selectedMention } = this.state;
+    const caretPos = this.textarea.selectionStart;
+    let content = this.state.content;
+    content = `${content.slice(0, pos)}${mentions[selectedMention]} ${content.slice(caretPos, content.length)}`;
+    this.onChange(content);
+    active = false;
+    pos = 0;
+    this.setState({ selectedMention: 0 });
+  }
   onChange(value) {
-    this.buildMentions(value);
     this.setState({ content: value });
     let content = value;
     let caretPos = 0;
     if (this.textarea) {
       caretPos = this.textarea.selectionStart;
     }
+    this.buildMentions(value, caretPos);
     this.previewNode.scrollTop = this.textarea.scrollTop;
     content = content.substr(0, caretPos);
     content = ContentEditable.moveCaret(content, caretPos);
     this.setState({ previewContent: content });
   }
-  buildMentions(text) {
-    const lastChar = text.slice(-1);
+  buildMentions(text, caretPos) {
+    const lastChar = text.charAt(caretPos - 1);
     const { users } = this.props;
     if (lastChar === ' ' || lastChar === '') {
       active = false;
       pos = 0;
     }
     if (lastChar === '@') {
-      pos = text.length;
+      pos = caretPos;
       active = true;
     }
     const mentions = new Set();
     if (active) {
-      const searchText = text.substring(pos, text.length);
+      const searchText = text.substring(pos, caretPos);
       const searchTextArray = Array.from(searchText); // Thanks ES6
       if (searchTextArray.length === 0) {
-        mentions.add(users);
+        for (let k = 0; k < users.length; k += 1) {
+          if (mentions.size > 9) {
+            break;
+          }
+          mentions.add(users[k]);
+        }
       } else {
         for (let i = 0; i < users.length; i += 1) {
+          if (mentions.size > 9) {
+            break;
+          }
           let candidate = true;
           for (let j = 0; j < searchTextArray.length; j += 1) {
-            if (users[i].indexOf(searchTextArray[j]) === -1) {
+            if (users[i].toLowerCase().indexOf(searchTextArray[j].toLowerCase()) === -1) {
               candidate = false;
             }
           }
@@ -102,7 +182,7 @@ class ContentEditable extends Component {
     this.onChange(content);
   }
   render() {
-    const { pickerActive } = this.state;
+    const { pickerActive, selectedMention } = this.state;
     return (
       <div
         className={`content_editable ${this.props.className}`}
@@ -121,8 +201,11 @@ class ContentEditable extends Component {
           dangerouslySetInnerHTML={{ __html: this.state.previewContent }}
         />
         <div id="mentions">
-          {this.state.mentions.map(mention => (
-            <div className="mention" key={mention}><p>{mention}</p></div>
+          {this.state.mentions.map((mention, index) => (
+            <div
+              className={`mention ${index === selectedMention ? 'active' : ''}`}
+              key={mention}
+            ><p>{mention}</p></div>
           ))}
         </div>
         <TwemojiComp
