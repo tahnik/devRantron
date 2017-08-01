fb8e64fc0d76bfd086097a8ceeaeb08242d51cd9
Tahnik Mustasin <tahnik@live.co.uk>
2017-08-01 01:46:13 -0700

    Clicking quit from the dock icon now exits the app #131

--- app/src/main/app.js
+++ app/src/main/app.js
@@ -205,6 +205,12 @@ app.on('activate', () => {
   }
 });
 
+if (process.platform === 'darwin') {
+  app.on('before-quit', function() {
+    quitApp();
+  });
+}
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
