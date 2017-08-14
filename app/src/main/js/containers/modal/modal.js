b434ecfc3ced278b16c57801f589f27771191461
Tahnik Mustasin <tahnik@live.co.uk>
2017-08-14 21:10:38 +0600

    fixed small bug

--- app/src/main/js/containers/modal/modal.js
+++ app/src/main/js/containers/modal/modal.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import Modal from '../../components/modal/modal';
 import vote from '../../actions/vote';
 import { fetchNotifs, clearNotif } from '../../actions/notifs';
@@ -33,4 +34,4 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Modal);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Modal));
