dbba363c777767a46b6de964ce829b82a10b8dd7
Sven Lechner <SirWindfield@users.noreply.github.com>
2018-04-16 22:34:31 +0200

    test(reducers): add search reducer tests

---
+++ app/src/js/reducers/__tests__/search.test.js
@@ -0,0 +1,23 @@
+import defaultStates from '../../consts/default_states';
+import { SEARCH } from '../../consts/types';
+import reduce from '../search';
+
+const defaultState = [];
+
+describe('[Reducer] search', () => {
+  it('should return the default state', () => {
+    const newState = reduce(defaultState, {});
+    expect(newState).toEqual(defaultState);
+  });
+  it('should handle ADDTOFREQ', () => {
+    const action = {
+      type: SEARCH.ADDTOFREQ,
+      term: 'something',
+    };
+
+    const newState = reduce(defaultState, action);
+    const expected = ['something'];
+
+    expect(newState).toEqual(expected);
+  });
+});
